e51c61fb8eadf874af4ddc5e55140c1d8defc98e
livialyrio <139237217+livialyrio@users.noreply.github.com>
2023-11-06 20:27:05 -0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,33 @@
+package model;
+public class Player {
+    private String name;
+    private String surname;
+    private Integer number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+
+
+}
